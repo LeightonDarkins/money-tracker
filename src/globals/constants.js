8cb00d516a8cbb830b090b7473ab4a9057118491
Leighton Darkins <ldarkins@thoughtworks.com>
2017-08-08 22:19:33 +0200

    LD - Add ENV Variable For Application Environment

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,8 +1,16 @@
 const HOST = process.env.MONEY_TRACKER_SERVER_HOST
 const PORT = process.env.MONEY_TRACKER_SERVER_PORT
+const ENV = process.env.MONEY_TRACKER_ENV
+let REQUEST_PREFIX
+
+if (ENV === 'development') {
+  REQUEST_PREFIX = 'http'
+} else {
+  REQUEST_PREFIX = 'https'
+}
 
 module.exports = {
   MINIMUM_ACCOUNT_NAME_LENGTH: 3,
   EMPTY_STRING: '',
-  SERVER_URI: `http://${HOST}:${PORT}`
+  SERVER_URI: `${REQUEST_PREFIX}://${HOST}:${PORT}`
 }
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ var config = {
     new ExtractTextPlugin('styles.css'),
     new webpack.DefinePlugin({
       'process.env':{
+        'MONEY_TRACKER_ENV': JSON.stringify(process.env.MONEY_TRACKER_ENV),
         'MONEY_TRACKER_SERVER_HOST': JSON.stringify(process.env.MONEY_TRACKER_SERVER_HOST),
         'MONEY_TRACKER_SERVER_PORT': JSON.stringify(process.env.MONEY_TRACKER_SERVER_PORT)
       }
